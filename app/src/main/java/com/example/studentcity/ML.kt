17fdce82523a71b8d6a560078c9ec6407ff0803f
lalumiere_17 <sveta.sukina@mail.ru>
2019-05-16 23:11:28 +0500

    CameraActivity changed
    Added:
    - camera permissions
    - intent for using camera
    - getting bitmap from camera

--- app/src/main/java/com/example/studentcity/ML.kt
+++ app/src/main/java/com/example/studentcity/ML.kt
@@ -19,52 +19,54 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
 
 class ML (mediaImage: Image) {
 
-    private val ORIENTATIONS = SparseIntArray()
+    val image = FirebaseVisionImage.fromBitmap(bitmap)
 
-    init {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90)
-        ORIENTATIONS.append(Surface.ROTATION_90, 0)
-        ORIENTATIONS.append(Surface.ROTATION_180, 270)
-        ORIENTATIONS.append(Surface.ROTATION_270, 180)
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Throws(CameraAccessException::class)
-    private  fun getRotationCompensation(cameraId: String, activity: Activity, context: Context) : Int {
-        val deviceRotation = activity.windowManager.defaultDisplay.rotation
-        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
-
-        val cameraManager = context.getSystemService(CAMERA_SERVICE) as CameraManager
-        val sensorOrientation = cameraManager
-            .getCameraCharacteristics(cameraId)
-            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
-        rotationCompensation = (rotationCompensation+sensorOrientation+270)%360
-        val result: Int
-        when (rotationCompensation) {
-            0 -> result = FirebaseVisionImageMetadata.ROTATION_0
-            90 -> result = FirebaseVisionImageMetadata.ROTATION_90
-            180 -> result = FirebaseVisionImageMetadata.ROTATION_180
-            270 -> result = FirebaseVisionImageMetadata.ROTATION_270
-            else -> {
-                result = FirebaseVisionImageMetadata.ROTATION_0
-                Log.e(TAG, "Bad rotation value: $rotationCompensation")
-            }
-        }
-        return result
-    }
-
-    val image = FirebaseVisionImage.fromMediaImage(mediaImage, rotation)
-
-    val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
-
-    val result = detector.processImage(image)
-        .addOnSuccessListener {
-                firebaseVisionText ->
-            //
-        }
-        .addOnFailureListener{
-
-        }
+//    private val ORIENTATIONS = SparseIntArray()
+//
+//    init {
+//        ORIENTATIONS.append(Surface.ROTATION_0, 90)
+//        ORIENTATIONS.append(Surface.ROTATION_90, 0)
+//        ORIENTATIONS.append(Surface.ROTATION_180, 270)
+//        ORIENTATIONS.append(Surface.ROTATION_270, 180)
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    @Throws(CameraAccessException::class)
+//    private  fun getRotationCompensation(cameraId: String, activity: Activity, context: Context) : Int {
+//        val deviceRotation = activity.windowManager.defaultDisplay.rotation
+//        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+//
+//        val cameraManager = context.getSystemService(CAMERA_SERVICE) as CameraManager
+//        val sensorOrientation = cameraManager
+//            .getCameraCharacteristics(cameraId)
+//            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+//        rotationCompensation = (rotationCompensation+sensorOrientation+270)%360
+//        val result: Int
+//        when (rotationCompensation) {
+//            0 -> result = FirebaseVisionImageMetadata.ROTATION_0
+//            90 -> result = FirebaseVisionImageMetadata.ROTATION_90
+//            180 -> result = FirebaseVisionImageMetadata.ROTATION_180
+//            270 -> result = FirebaseVisionImageMetadata.ROTATION_270
+//            else -> {
+//                result = FirebaseVisionImageMetadata.ROTATION_0
+//                Log.e(TAG, "Bad rotation value: $rotationCompensation")
+//            }
+//        }
+//        return result
+//    }
+//
+//    val image = FirebaseVisionImage.fromMediaImage(mediaImage, rotation)
+//
+//    val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+//
+//    val result = detector.processImage(image)
+//        .addOnSuccessListener {
+//                firebaseVisionText ->
+//            //
+//        }
+//        .addOnFailureListener{
+//
+//        }
 
 
 
